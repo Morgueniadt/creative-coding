5d07ebaa962f702f8f3dfa201fa52e5ce92164bf
Morgueniadt <N00230651@student.iadt.ie>
2025-03-20 15:08:23 +0000

    life coutner and alert counter added

--- classes/Airfield.js
+++ classes/Airfield.js
@@ -3,28 +3,29 @@ class Airfield {
         this.numAirCraft = obj.numAirCraft ?? 10;
         this.airFieldWidth = obj.airFieldWidth ?? 500;
         this.airFieldHeight = obj.airFieldHeight ?? 500;
-        this.airFieldPosX =  obj.airFieldPosX ?? 250;
+        this.airFieldPosX = obj.airFieldPosX ?? 250;
         this.airFieldPosY = obj.airFieldPosY ?? 250;
         this.airCrafts = [];
+        this.lives = 3;  // Starting lives
+        this.alertCount = 0;  // Starting alert counter
         this.generateAirCraft();
-        
     }
-    
+
     renderAirfield() {
         push();
         translate(this.airFieldPosX, this.airFieldPosY);
         fill(100);
-        rect(0,0,this.airFieldWidth, this.airFieldHeight)
-        pop()
+        rect(0, 0, this.airFieldWidth, this.airFieldHeight);
+        pop();
     }
 
     renderAirCraft() {
         push();
         translate(this.airFieldPosX, this.airFieldPosY);
-        fill(0,255,255)
-        this.airCrafts.forEach((airCraft,id) => {
+        fill(0, 255, 255);
+        this.airCrafts.forEach((airCraft, id) => {
             airCraft.renderAirCraft(id);
-        })
+        });
         pop();
     }
 
@@ -36,59 +37,52 @@ class Airfield {
     }
 
     generateAirCraft() {
-        for(let i = 0; i < this.numAirCraft; i++) {
-            let gen = random(0,1)
+        for (let i = 0; i < this.numAirCraft; i++) {
+            let gen = random(0, 1);
             if (gen < 0.5) {
-                 this.airCrafts.push(new AirCraft({
-            posx: random(0, this.airFieldWidth),
-            posy: random(0, this.airFieldHeight)
-        }));
-            } else{
+                this.airCrafts.push(new AirCraft({
+                    posx: random(0, this.airFieldWidth),
+                    posy: random(0, this.airFieldHeight)
+                }));
+            } else {
                 this.airCrafts.push(new Heli({
                     posx: random(0, this.airFieldWidth),
                     posy: random(0, this.airFieldHeight)
                 }));
             }
-           
         }
     }
-    
-checkDist() {
-    this.airCrafts.forEach(airCraft => airCraft.alert = 0); // Reset alerts before checking
 
-    for (let i = 0; i < this.airCrafts.length; i++) {
-        for (let j = i + 1; j < this.airCrafts.length; j++) {
-            let AirCraftA = this.airCrafts[i];
-            let AirCraftB = this.airCrafts[j];
-
-            // Calculate the Euclidean distance between aircraft
-            let distBetween = dist(AirCraftA.pos.x, AirCraftA.pos.y, AirCraftB.pos.x, AirCraftB.pos.y);
-
-            // Collision alert threshold (adjustable)
-            let minSafeDist = (AirCraftA.apWidth + AirCraftB.apWidth) / 2 + 10; // Buffer of 10
-
-            if (distBetween < minSafeDist) {
-                AirCraftA.alert = 1;
-                AirCraftB.alert = 1;
+    checkDist() {
+        this.airCrafts.forEach(airCraft => airCraft.alert = 0);
+        this.count = 0;
+        for (let i = 0; i < this.airCrafts.length; i++) {
+            for (let j = i + 1; j < this.airCrafts.length; j++) {
+                let AirCraftA = this.airCrafts[i];
+                let AirCraftB = this.airCrafts[j];
+                let dist = sqrt((sq(AirCraftA.pos.x - AirCraftB.pos.x)) + (sq(AirCraftA.pos.y - AirCraftB.pos.y)));
+                if (dist < 50) {
+                    AirCraftA.alert = 1;
+                    AirCraftB.alert = 1;
+                    this.alertCount++;  // Increment the alert counter
+                }
+                this.count++;
             }
         }
     }
-        //console.log(this.count)
-        
-    }
 
     checkLimit(airCraft) {
-    if (airCraft.pos.x > this.airFieldWidth) {
-        airCraft.pos.x = 0;
-    } else if (airCraft.pos.x < 0) {
-        airCraft.pos.x = this.airFieldWidth;
+        if (airCraft.pos.x > this.airFieldWidth) {
+            airCraft.pos.x = 0;
+        } else if (airCraft.pos.x < 0) {
+            airCraft.pos.x = this.airFieldWidth;
+        }
+    
+        if (airCraft.pos.y > this.airFieldHeight) {
+            airCraft.pos.y = 0;
+        } else if (airCraft.pos.y < 0) {
+            airCraft.pos.y = this.airFieldHeight;
+        }
     }
-
-    if (airCraft.pos.y > this.airFieldHeight) {
-        airCraft.pos.y = 0;
-    } else if (airCraft.pos.y < 0) {
-        airCraft.pos.y = this.airFieldHeight;
-    }
-}
-
+    
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 let airFields = [];
-let currentAirCraft = 0
+let currentAirCraft = 0;
 
-function setup () {
-    createCanvas(1000,1000);
-    angleMode(DEGREES)
+function setup() {
+    createCanvas(1000, 1000);
+    angleMode(DEGREES);
 
     airFields.push(new Airfield({
         numAirCraft: 10,
@@ -11,52 +11,40 @@ function setup () {
         airFieldHeight: 1000,
         airFieldPosX: 0,
         airFieldPosY: 0
-        
-        
     }));
-
-
 }
+
 function draw() {
-    background(100,225,100);
-    for(let i = 0; i < airFields.length; i++) {
-    airFields[i].renderAirfield();
-    airFields[i].renderAirCraft();
-    airFields[i].moveAirCraft();
-    airFields[i].checkDist();
+    background(100, 225, 100);
+    
+    for (let i = 0; i < airFields.length; i++) {
+        airFields[i].renderAirfield();
+        airFields[i].renderAirCraft();
+        airFields[i].moveAirCraft();
+        airFields[i].checkDist();
     }
-    if (keyIsDown(65)){
-        airFields[0].airCrafts[0].turnLeft()
+
+    // Display Lives and Alert Count in the top left corner
+    displayStats();
+
+    if (keyIsDown(65)) {
+        airFields[0].airCrafts[0].turnLeft();
     }
     if (keyIsDown(68)) {
-        airFields[0].airCrafts[0].turnRight()
+        airFields[0].airCrafts[0].turnRight();
     }
     if (keyIsDown(87)) {
-        airFields[0].airCrafts[0].increaseSpeed()
+        airFields[0].airCrafts[0].increaseSpeed();
     }
     if (keyIsDown(83)) {
-        airFields[0].airCrafts[0].decreaseSpeed()
+        airFields[0].airCrafts[0].decreaseSpeed();
     }
-
-
 }
 
-// function keyPressed() {
+function displayStats() {
+    fill(255);
+    textSize(18);
+    text("Lives: " + airFields[0].lives, 10, 30);
+    text("Alert Count: " + airFields[0].alertCount, 10, 60);
+}
 
-//     if (key === "A") {
-//         airFields[0].airCrafts[0].turnLeft()
-//     }
-//     if (key === "D") {
-//         airFields[0].airCrafts[0].turnRight()
-//     }
-//     if (key === "W") {
-//         airFields[0].airCrafts[0].increaseSpeed()
-//     }
-//     if (key === "S") {
-//         airFields[0].airCrafts[0].decreaseSpeed()
-//     }
-
-
-
-
-// }
