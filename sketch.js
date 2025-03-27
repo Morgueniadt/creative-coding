16a2f48fd90a39064c4bad6b8c18eb69e05add53
Morgueniadt <N00230651@student.iadt.ie>
2025-03-27 16:55:34 +0000

    Refactor collision handling and add explosion debris, removed second draw only rendering the asteroids

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var aircraft;
 var asteroids = [];
 var lasers = [];
+let debris = []
 var stats;
 var gameOver = false; // Track if the game is over
 
@@ -32,50 +33,74 @@ function draw() {
   }
 
   // Loop through all asteroids and check for collisions with the aircraft
-  for (var i = 0; i < asteroids.length; i++) {
+  for (let i = asteroids.length - 1; i >= 0; i--) {
     if (aircraft.hits(asteroids[i])) {
       stats.health -= 10; // Decrease health if aircraft hits an asteroid
-      console.log('Aircraft hit an asteroid! Health:', stats.health);
 
-      // If health is zero or below, stop the game
+      // Add debris when the aircraft hits an asteroid
+      for (let k = 0; k < 30; k++) {
+        debris.push(
+          new Debris(asteroids[i].pos.copy(), p5.Vector.random2D().mult(random(1, 3)))
+        );
+      }
+
       if (stats.health <= 0) {
-        gameOver = true; // Set the game over flag
+        gameOver = true;
         noLoop(); // Stop the game loop
-        break; // Exit the loop early
+        break;
       }
 
       // Break the asteroid into smaller pieces
       let newAsteroids = asteroids[i].breakup();
-      asteroids.splice(i, 1, ...newAsteroids); // Replace the old asteroid with new smaller asteroids
+      asteroids.splice(i, 1, ...newAsteroids);
     }
-    asteroids[i].render();
+
+    // Update and render each asteroid
     asteroids[i].update();
+    asteroids[i].render();
     asteroids[i].edges();
   }
 
   // Loop through all lasers
-  for (var i = lasers.length - 1; i >= 0; i--) {
+  for (let i = lasers.length - 1; i >= 0; i--) {
     lasers[i].render();
     lasers[i].update();
+
     if (lasers[i].offscreen()) {
       lasers.splice(i, 1);
     } else {
-      for (var j = asteroids.length - 1; j >= 0; j--) {
+      for (let j = asteroids.length - 1; j >= 0; j--) {
         if (lasers[i].hits(asteroids[j])) {
+          // Add debris when laser hits an asteroid
+          for (let k = 0; k < 20; k++) {
+            debris.push(
+              new Debris(asteroids[j].pos.copy(), p5.Vector.random2D().mult(random(1, 3)))
+            );
+          }
+
           if (asteroids[j].r > 10) {
-            var newAsteroids = asteroids[j].breakup();
+            let newAsteroids = asteroids[j].breakup();
             asteroids = asteroids.concat(newAsteroids);
           }
           asteroids.splice(j, 1);
           lasers.splice(i, 1);
-          stats.score += 10; // Increase score when asteroid is destroyed
-          stats.accuracy = calculateAccuracy(lasers.length); // Update accuracy
+          stats.score += 10;
           break;
         }
       }
     }
   }
 
+  // Update and render debris
+  for (let i = debris.length - 1; i >= 0; i--) {
+    debris[i].update();
+    debris[i].render();
+    if (debris[i].isDead()) {
+      debris.splice(i, 1);
+    }
+  }
+
+  // Render and update the aircraft
   aircraft.render();
   aircraft.turn();
   aircraft.update();
@@ -83,7 +108,7 @@ function draw() {
 
   // Update stats with the current values
   stats.update(stats.accuracy, stats.score, stats.health);
-  stats.render(); // Render stats on the screen
+  stats.render();
 }
 
 // Function to display a Game Over message and prompt to try again
@@ -125,26 +150,4 @@ function keyPressed() {
     loop(); // Restart the game loop
   }
 }
-function draw() {
-  background(0);
 
-  // Update and render asteroids
-  for (let i = asteroids.length - 1; i >= 0; i--) {
-    asteroids[i].update();
-    asteroids[i].render();
-    asteroids[i].edges();
-
-    if (asteroids[i].isDead()) {
-      asteroids.splice(i, 1); // Remove destroyed asteroids
-    }
-  }
-
-  // Update and render debris
-  for (let i = debris.length - 1; i >= 0; i--) {
-    debris[i].update();
-    debris[i].render();
-    if (debris[i].isDead()) {
-      debris.splice(i, 1);
-    }
-  }
-}
