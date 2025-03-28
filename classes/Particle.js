ef1026f093bec41bd9281d1a4dab1d831efe2ef8
Morgueniadt <N00230651@student.iadt.ie>
2025-03-28 14:28:13 +0000

    added in particle movement and generstion explannations

--- classes/Particle.js
+++ classes/Particle.js
@@ -1,24 +1,24 @@
 class Particle {
     constructor(pos) {
-      this.pos = pos.copy();
-      this.vel = p5.Vector.random2D().mult(random(1, 3));
-      this.lifetime = 255;
+      this.pos = pos.copy(); // Create a copy of the position so we don't modify the original
+      this.vel = p5.Vector.random2D().mult(random(0.1, 2)); // Random velocity vector with magnitude between 1 and 3
+      this.lifetime = 255; 
     }
   
     update() {
-      this.pos.add(this.vel);
+      this.pos.add(this.vel); // Update position based on velocity vector 
       this.lifetime -= 5;
     }
   
     render() {
       push();
-      stroke(255, this.lifetime);
-      point(this.pos.x, this.pos.y);
-      pop();
+      stroke(255, this.lifetime);  // Set stroke color with alpha value based on lifetime
+      point(this.pos.x, this.pos.y); // Draw a point at the particle's position
+      pop();   
     }
   
     isFinished() {
-      return this.lifetime <= 0;
+      return this.lifetime <= 0; // Return true if lifetime is less than or equal to 0
     }
   }
   
